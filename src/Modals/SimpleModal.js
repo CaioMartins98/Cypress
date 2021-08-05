fa865e73052948f9f776fded4915280f6371509a
Segware <cdias@segware.com>
2021-08-05 15:57:38 -0300

    Studying about Cypress

--- src/Modals/SimpleModal.js
+++ src/Modals/SimpleModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
 import AppsIcon from "@material-ui/icons/Apps";
 import Modal from "@material-ui/core/Modal";
@@ -31,7 +31,8 @@ const useStyles = makeStyles((theme) =>
       padding: theme.spacing(2, 4, 3),
       display: "flex",
       alignItems: "center",
-      justifyContent: "space-around",
+      justifyContent: "center",
+      flexDirection: 'column'
     },
     divImg: {
       width: 200,
@@ -51,6 +52,7 @@ const useStyles = makeStyles((theme) =>
 );
 
 export default function SimpleModal() {
+const[clicked, setClicked] = useState(false)
   const classes = useStyles();
   // getModalStyle is not a pure function, we roll the style only on the first render
   const [modalStyle] = React.useState(getModalStyle);
@@ -64,10 +66,34 @@ export default function SimpleModal() {
     setOpen(false);
   };
 
+  const handleClick = () => {
+      alert('Cadastro realizado com sucesso!')
+  }
   // const imagem = url('https://ssl.gstatic.com/gb/images/p1_c9bc74a1.png');
   const body = (
     <div style={modalStyle} className={classes.paper}>
-      <h5>Hello World! I'm a Modal!</h5>
+      <label>
+        Nome: 
+        <input type='text' id='name' placeholder='Digite seu nome'/>
+      </label>
+      <label>
+        Idade: 
+        <input type='text' id='age' placeholder='Digite sua idade'/>
+      </label>
+      <label>
+        CPF: 
+        <input type='text' id='cpf' placeholder='Digite seu CPF'/>
+      </label>
+      <label>
+        Nascimento: 
+        <input type='text' id='date'/>
+      </label>
+      <label>
+        Senha: 
+        <input type='password' id='pass' placeholder='Digite sua senha'/>
+      </label>
+      <button type='submit' onClick={()=> setClicked(true)}>ENVIAR</button>
+      {clicked && <p id='success' style={{color:'green'}}>Cadastro realizado com sucesso!</p>}
     </div>
   );
 
--- src/Modals/SimpleModal.test.js
+++ src/Modals/SimpleModal.test.js
@@ -1,10 +1,19 @@
 /* eslint-disable no-undef */
-import React from 'react'
-import {mount} from '@cypress/react'
-import SimpleModal from './SimpleModal';
+import React from "react";
+import { mount } from "@cypress/react";
+import SimpleModal from "./SimpleModal";
 
-it('should open the modal when button is clicked', () => {
-  mount(<SimpleModal/>);
-  cy.viewport(1080, 760)
-  cy.get('button').click()
-})
+it("should open the modal when button is clicked", () => {
+  mount(<SimpleModal />);
+  cy.viewport(1080, 760);
+  cy.get("button").click();
+
+  cy.get("#name").should("be.visible").type("Caio Martins Dias");
+  cy.get("#age").should("be.visible").type("23");
+  cy.get("#cpf").should("be.visible").type("123.456.789-10");
+  cy.get("#date").should("be.visible").type("17/01/1998");
+  cy.get("#pass").should("be.visible").type("12345678");
+  cy.get('button[type="submit"]').should("be.visible").click()
+  cy.get('#success').should('be.visible')
+  
+});
